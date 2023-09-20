f723115fa0d7e0368aa409ea4502724e5f5ed39e
Yamileth Reveca <yamilethcaldera@gmail.com>
2023-09-20 02:11:24 -0300

    Analizando texto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,107 @@
 const analyzer = {  
   getWordCount: (text) => {
+
+    let array= text.trim();
+    
+    if(array===""){
+      return 0;
+   
+    }else{
+
+      array= array.split(" ");
+      return array.length;
+
+    }
+    // retorna el texto palabra por palabra.
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+
+    return text.length;
+    
+    // retorna los caracteres tal como llega sin espacio, con todo los signos de puntuación.
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+   let eliminarCaracteres= text.replace(/[^A-Za-z0-9]/g, "");
+   console.log(eliminarCaracteres);
+   eliminarCaracteres=eliminarCaracteres.replace(/\s+/g, "");
+   console.log(eliminarCaracteres);
+
+   return eliminarCaracteres.length;
+
+    // retorna los caracteres sin espacio - excluye los signos de puntuacion y los espacios
+   //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {  
+   
+    let eliminarCaracteres= text.replace(/\.\,\:;_-\!\?°\|"#\$%&\/\(\)\=\'¡¿¨\^\+\*\}\{\]\[\'/g, "");
+    eliminarCaracteres=eliminarCaracteres.replace(/\s+/g, "");
+   
+    let array= text.trim();
+    
+    if(array===""){
+      return 0;
+   
+    }else{
+
+      array= array.split(" ");
+      return eliminarCaracteres.length/array.length;
+
+    }
+        
+    // aqui retorna la longitud de medición entre los caracteres sin espacio entre las cantidades de palabras.
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    
+    
+    let numeros= text.trim();
+    console.log("mira trim "+ numeros)    
+    let reemplazonumeros=numeros.replace(/[^0-9\s]+/g, "");
+    
+    console.log("reemplazo "+ numeros)   
+    
+    if(reemplazonumeros===""){
+
+      return 0;
+    }else{
+
+      reemplazonumeros= reemplazonumeros.split(/\s/g);
+      console.log("reemplazo 2 "+ reemplazonumeros)   
+      return reemplazonumeros.length;
+    }
+    
+
+    
+    
+    
+      // retorna los numeros por cifras y dara la cantidad de longitud. 
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+   
+    // let sumaNumeros= 0;
+    //   sumaNumeros= text.trim().split(" ");
+
+    //   for(let i= 0; i<text.length; i++){
+
+    //   const regex=/^[0-9]*$/;
+    //   let onlyNumber=regex.test(text[i]);
+    //   // 
+    //     if(onlyNumber===true){
+    //       sumaNumeros= sumaNumeros , parseInt(text);
+    //       console.log("suma de numeros"+ sumaNumeros);
+          
+    //     }
+        
+    // }
+
+   
+    // return sumaNumeros;
+   
+    // retora los numeros en fracciones y sumandose entre ellas.
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import analyzer from './analyzer.js';
 
+// aqui inicio en 0 todo los li 
+
+let contador= 0;
+
+document.querySelector(".palabras").innerHTML="Palabras:"+ contador;
+document.querySelector(".caracteres").innerHTML="Caracteres:"+ contador;
+document.querySelector(".sincaracteres").innerHTML="Carácter sin espacios:"+ contador;
+document.querySelector(".numeros").innerHTML="Números:"+ contador;
+document.querySelector(".sumanumeros").innerHTML="Suma números:"+ contador;
+document.querySelector(".promediolongitud").innerHTML="Promedio longitud:"+ contador;
+
+
+
+// aqui se escucha el eventos input del textarea
+
+let textarea = document.querySelector("textarea[name='user-input']");
+textarea.addEventListener("input",calcularValores);
+
+function calcularValores(e) {
+  
+  document.querySelector(".palabras").innerHTML="Palabras:"+ analyzer.getWordCount(e.target.value);
+  document.querySelector(".caracteres").innerHTML="Caracteres:"+ analyzer.getCharacterCount(e.target.value);
+  document.querySelector(".sincaracteres").innerHTML="Carácter sin espacios:"+ analyzer.getCharacterCountExcludingSpaces(e.target.value);
+  document.querySelector(".numeros").innerHTML="Números:"+ analyzer.getNumberCount(e.target.value);
+  document.querySelector(".sumanumeros").innerHTML="Suma números:"+ analyzer.getNumberSum(e.target.value);
+  document.querySelector(".promediolongitud").innerHTML="Promedio longitud:"+ analyzer.getAverageWordLength(e.target.value);
+}
+
+// boton limpiador de todos los li
+let boton= document.getElementById("reset-button");
+let limpiar= document.querySelector("textarea[name='user-input']");
+boton.addEventListener("click", function(){
+  limpiar.value="";
+  document.querySelector(".palabras").innerHTML="Palabras:"+ contador;
+  document.querySelector(".caracteres").innerHTML="Caracteres:"+ contador;
+  document.querySelector(".sincaracteres").innerHTML="Carácter sin espacios:"+ contador;
+  document.querySelector(".numeros").innerHTML="Números:"+ contador;
+  document.querySelector(".sumanumeros").innerHTML="Suma números:"+ contador;
+  document.querySelector(".promediolongitud").innerHTML="Promedio longitud:"+ contador;
+      
+})
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
